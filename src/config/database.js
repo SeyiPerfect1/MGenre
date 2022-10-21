b9e4002f6ac2a2aed4f82ff8230faf3933b80a1f
Oluseyi Adeegbe <adeegbeoluseyi@gmail.com>
2022-10-21 18:21:58 +0100

    database configuration

--- src/config/database.js
+++ src/config/database.js
@@ -15,13 +15,14 @@ if (process.env.NODE_ENV === "development") {
   sequelize = new Sequelize(process.env.DATABASE_URL);
 }
 
-try {
-  await sequelize.authenticate();
-  console.log("Connection has been established successfully.");
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
-  next(error);
-}
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 const db = {};
 
