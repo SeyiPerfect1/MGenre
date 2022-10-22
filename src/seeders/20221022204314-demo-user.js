2e085614c68401942253bbbcb1e7380256f13646
Oluseyi Adeegbe <adeegbeoluseyi@gmail.com>
2022-10-22 21:58:31 +0100

    seeding

--- src/seeders/20221022204314-demo-user.js
+++ src/seeders/20221022204314-demo-user.js
@@ -3,23 +3,23 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  await queryInterface.bulkInsert('Users', [{
+    firstname: "Oluseyi",
+    lastname: "Adeegbe",
+    username: "Oluperfect1",
+    mobile: "07067729339",
+    email: "adeegbeoluseyi@gmail.com",
+    password: "@Oluperfect1",
+    dob: "1996/11/23",
+    lastlogin: "2022/10/20",
+    introduction: 'I am a good boy',
+    country: "Nigeria",
+    userPicture: "user/users/pictures/oluseyi.png",
+    registeredAt: new Date()
+  }]);
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+   await queryInterface.bulkDelete("Users", null, {});
   }
 };
