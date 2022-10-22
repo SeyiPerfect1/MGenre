7028c13906e75d48cdfd483c2778f1cc6e8b941a
Oluseyi Adeegbe <adeegbeoluseyi@gmail.com>
2022-10-22 22:06:14 +0100

    code refactoring

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
-const  bodyParser = require('body-parser');
-const express = require('express');
-const dotenv = require('dotenv');
+const bodyParser = require("body-parser");
+const express = require("express");
+const dotenv = require("dotenv");
 dotenv.config();
-const { errorHandler } = require('./src/middlewares/errorHandler.js');
-const logger = require('morgan');
-const path = require('path');
-
+const { errorHandler } = require("./src/middlewares/errorHandler.js");
+const logger = require("morgan");
+const path = require("path");
 
 //instantiate express app
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //middleware to serve public files
 app.use(express.static(path.join(__dirname, "./src/public")));
 
 //invalid url route
-app.get('*', (req, res) => {
-    try{
-        res.sendFile(path.join(__dirname, './src/public/error.html'));
-    } catch(error){
-        next(error)
-    }
-  });
+app.get("*", (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname, "./src/public/error.html"));
+  } catch (error) {
+    next(error);
+  }
+});
 
 // add error middleware
-app.use(errorHandler())
+app.use(errorHandler());
 
 //create server
 app.listen(PORT, () => {
-    console.log(`server is listening on http://localhost:${PORT}`)
-})
-
-
-
-
-
-
+  console.log(`server is listening on http://localhost:${PORT}`);
+});
